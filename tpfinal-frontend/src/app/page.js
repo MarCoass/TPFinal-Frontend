c211fd6f6b1cb839d56830720198224980031134
Martina Coassin <76189888+MarCoass@users.noreply.github.com>
2023-08-18 10:20:37 -0300

    Update page.js

--- tpfinal-frontend/src/app/page.js
+++ tpfinal-frontend/src/app/page.js
@@ -1,12 +1,16 @@
-export default function Home() {
-  fetch("http://127.0.0.1:8000/ejemplo")
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error("Error:", error));
+const fetchEjemplo = () => {
+  return fetch("http://127.0.0.1:8000/ejemplo").then((response) => response.json());
+  
+};
 
+export default async function Home() {
+  const data = await fetchEjemplo();
+  console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>HOLI</p>
+      <div>{data.message}<br/>
+      {data.timestamp}</div>
     </main>
   );
 }
