6ccb8543f12e6c92615c6c13f355b6dd64c701ec
Martina Coassin <76189888+MarCoass@users.noreply.github.com>
2023-09-09 16:26:35 -0300

    cambios en el navbar

--- tpfinal-frontend/src/components/Layouts/AppLayout.js
+++ tpfinal-frontend/src/components/Layouts/AppLayout.js
@@ -2,10 +2,10 @@ import Navigation from '@/components/Layouts/Navigation'
 import { useAuth } from '@/hooks/auth'
 
 const AppLayout = ({ header, children }) => {
-    const { user } = useAuth({ middleware: 'auth' })
+    const { user } = useAuth()
 
     return (
-        <div className="min-h-screen bg-gray-100">
+        <div className="min-h-screen bg-naranja-100">
             <Navigation user={user} />
 
             {/* Page Heading */}
--- tpfinal-frontend/src/components/Layouts/Navigation.js
+++ tpfinal-frontend/src/components/Layouts/Navigation.js
@@ -18,7 +18,7 @@ const Navigation = ({ user }) => {
     const [open, setOpen] = useState(false)
 
     return (
-        <nav className="bg-cremita-200 border-b border-naranja-100">
+        <nav className="bg-white border-b border-gray-100">
             {/* Primary Navigation Menu */}
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
@@ -35,41 +35,72 @@ const Navigation = ({ user }) => {
                             <NavLink
                                 href="/dashboard"
                                 active={router.pathname === '/dashboard'}>
-                                Dashboard
+                                Tienda
+                            </NavLink>
+                        </div>
+                        <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <NavLink
+                                href="/pedidos"
+                                active={router.pathname === '/pedidos'}>
+                                Pedidos personalizados
+                            </NavLink>
+                        </div>
+                        <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <NavLink
+                                href="/informacion"
+                                active={router.pathname === '/informacion'}>
+                                Informacion
                             </NavLink>
                         </div>
                     </div>
-
                     {/* Settings Dropdown */}
-                    <div className="hidden sm:flex sm:items-center sm:ml-6">
-                        <Dropdown
-                            align="right"
-                            width="48"
-                            trigger={
-                                <button className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 focus:outline-none transition duration-150 ease-in-out">
-                                    <div>{user?.name}</div>
-                                    {console.log(user)}
-                                    <div className="ml-1">
-                                        <svg
-                                            className="fill-current h-4 w-4"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20">
-                                            <path
-                                                fillRule="evenodd"
-                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                clipRule="evenodd"
-                                            />
-                                        </svg>
-                                    </div>
-                                </button>
-                            }>
-                            {/* Authentication */}
-                            <DropdownButton onClick={logout}>
-                                Logout
-                            </DropdownButton>
-                        </Dropdown>
-                    </div>
-
+                    {user ? (
+                        <div className="hidden sm:flex sm:items-center sm:ml-6">
+                            <Dropdown
+                                align="right"
+                                width="48"
+                                trigger={
+                                    <button className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 focus:outline-none transition duration-150 ease-in-out">
+                                        <div>{user?.name}</div>
+                                        {console.log(user)}
+                                        <div className="ml-1">
+                                            <svg
+                                                className="fill-current h-4 w-4"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20">
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clipRule="evenodd"
+                                                />
+                                            </svg>
+                                        </div>
+                                    </button>
+                                }>
+                                {/* Authentication */}
+                                <DropdownButton onClick={logout}>
+                                    Logout
+                                </DropdownButton>
+                            </Dropdown>
+                        </div>
+                    ) : (
+                        <div className='flex'>
+                            <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                <NavLink
+                                    href="/login"
+                                    active={router.pathname === '/login'}>
+                                    Iniciar Sesion
+                                </NavLink>
+                            </div>
+                            <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                <NavLink
+                                    href="/login"
+                                    active={router.pathname === '/register'}>
+                                    Registrarse
+                                </NavLink>
+                            </div>
+                        </div>
+                    )}
                     {/* Hamburger */}
                     <div className="-mr-2 flex items-center sm:hidden">
                         <button
