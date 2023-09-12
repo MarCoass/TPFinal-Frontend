512134ecc6bee27b12e72afedd5fa63e2c6cf5b4
MartinaRosalesF <martina.m.rosales@gmail.com>
2023-09-12 19:24:48 -0300

    agregue un footer

--- tpfinal-frontend/src/components/Layouts/AppLayout.js
+++ tpfinal-frontend/src/components/Layouts/AppLayout.js
@@ -1,7 +1,7 @@
 import Navigation from '@/components/Layouts/Navigation'
 import { useAuth } from '@/hooks/auth'
 
-const AppLayout = ({ header, children }) => {
+const AppLayout = ({ header, children, footer }) => {
     const { user } = useAuth()
 
     return (
@@ -17,6 +17,13 @@ const AppLayout = ({ header, children }) => {
 
             {/* Page Content */}
             <main>{children}</main>
+
+            {/* Page footer */}
+            <footer className="bg-white shadow sticky bottom-0 z-50">
+                <div className="max-w-auto mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {footer}
+                </div>
+            </footer>
         </div>
     )
 }
