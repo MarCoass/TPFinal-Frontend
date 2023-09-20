23e494a40fbf2f1894ba531fd507c6edd39bc49c
MartinaRosalesF <martina.m.rosales@gmail.com>
2023-09-19 23:00:17 -0300

    agregue datos a las tarjetas

--- tpfinal-frontend/src/components/ProductCard.js
+++ tpfinal-frontend/src/components/ProductCard.js
@@ -1,24 +1,27 @@
 import React from 'react';
-const ProductCard = ({imgUrl, nombreProducto, precioProducto}) => {
+
+const ProductCard = ({imgUrl, nombreProducto, descripcionProducto, precioProducto, stock}) => {
 return (
   
-   <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
-      <div class="overflow-x-hidden rounded-2xl relative">
-        <img class="h-40 rounded-2xl w-full object-cover" src={imgUrl}></img>
-        <button class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+   <div className="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+      <div className="overflow-x-hidden rounded-2xl relative">
+        <img className="h-40 rounded-2xl w-full object-cover" src={imgUrl}></img>
+        <button className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
           </svg>
         </button>
       </div>
-      <div class="mt-4 pl-2 mb-2 flex justify-between ">
+      <div className="mt-4 pl-2 mb-2 flex justify-between ">
         <div>
-          <p class="text-lg font-semibold text-gray-900 mb-0">{nombreProducto}</p>
-          <p class="text-md text-gray-800 mt-0">{precioProducto}</p>
+          <p className="text-lg font-semibold text-gray-900 mb-0">{nombreProducto}</p>
+          <p className="text-lg font-semibold text-gray-900 mb-0">{descripcionProducto}</p>
+          <p className="text-md text-gray-800 mt-0">${precioProducto}</p>
+          { stock ? (<p className="text-md text-gray-800 mt-0">Stock: {stock}</p>):(null)}
         </div>
-        <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+        <div className="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
           </svg>
         </div>
       </div>
--- tpfinal-frontend/src/pages/tienda/catalogo/index.js
+++ tpfinal-frontend/src/pages/tienda/catalogo/index.js
@@ -2,19 +2,62 @@ import AppLayout from '@/components/Layouts/AppLayout'
 import Head from 'next/head'
 import StoreLayout from '../layout'
 import ProductCard from '@/components/ProductCard'
+import React, { useEffect, useState } from 'react'
+import axios from '@/lib/axios'
+
+const fetchProductos = () => {
+    return axios
+        .get(
+            process.env.NEXT_PUBLIC_BACKEND_URL + '/administracion/productos', // Usa axios.get en lugar de fetch
+        )
+        .then(res => res.data)
+}
+
 const Catalogo = () => {
+    const [productos, setProductos] = useState(null)
+    useEffect(() => {
+        async function obtenerProductos() {
+            try {
+                const data = await fetchProductos()
+                setProductos(data)
+            } catch (error) {
+                console.error('Error al obtener productos:', error)
+            }
+        }
+        obtenerProductos()
+    }, [])
+
+    if (productos === null) {
+        // Puedes mostrar un mensaje de carga mientras esperas que se resuelva la Promise
+        return <div>Cargando productos...</div>
+    }
+
     return (
 
-         <StoreLayout>
+        <StoreLayout>
             <Head>
                 <title>Catalogo - Mar Nails</title>
             </Head>
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
-                            <ProductCard></ProductCard>
+                    <div className="container bg-white overflow-hidden shadow-sm sm:rounded-lg sm:px-6 lg:px-8">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4 p-6 bg-white border-b border-gray-200">
+                            {productos.map(producto => (
+                                <div key={producto.id}>
+                                    {/* <p>Nombre: {producto.nombre}</p>
+                                    <p>Descripcion: {producto.descripcion}</p>
+                                    <p>Precio: {producto.precio}</p>
+                                    <p>Stock: {producto.stock}</p> */}
+                                    <ProductCard
+                                        imgUrl={producto.url_imagen}
+                                        nombreProducto={producto.nombre}
+                                        descripcionProducto={producto.descripcion}
+                                        precioProducto={producto.precio}
+                                    >
+                                    </ProductCard>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
