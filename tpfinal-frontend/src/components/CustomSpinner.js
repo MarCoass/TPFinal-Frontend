92385457ba905524730148ebacdef35a8b422b0c
MartinaRosalesF <martina.m.rosales@gmail.com>
2023-10-03 19:07:33 -0300

    instale shadcn

--- tpfinal-frontend/src/components/CustomSpinner.js
+++ tpfinal-frontend/src/components/CustomSpinner.js
@@ -1,10 +1,10 @@
 export default function CustomSpinner({ mensaje }) {
   return (
     <div
-      class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent text-violeta-600 align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent text-violeta-600 align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
       role="status">
       <span
-        class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+        className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
       >{mensaje}</span>
     </div>
 
--- tpfinal-frontend/tailwind.config.js
+++ tpfinal-frontend/tailwind.config.js
@@ -1,77 +1,135 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
-const { nextui } = require('@nextui-org/react')
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './src/**/*.js',
-        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-    ],
-    darkMode: 'class',
-    plugins: [nextui()],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-            colors: {
-                naranja: {
-                    50: '#FFF2E5',
-                    100: '#FFE5CC',
-                    200: '#FFCA99',
-                    300: '#FFB066',
-                    400: '#FF9633',
-                    500: '#FF7D00',
-                    600: '#CC6300',
-                    700: '#994A00',
-                    800: '#663100',
-                    900: '#331900',
-                    950: '#190C00',
-                },
-                cremita: {
-                    50: '#FFFDFA',
-                    100: '#FFFBF5',
-                    200: '#FFF7EB',
-                    300: '#FFF4E5',
-                    400: '#FFF0DB',
-                    500: '#FFECD1',
-                    600: '#FFC675',
-                    700: '#FF9F1A',
-                    800: '#B86B00',
-                    900: '#5C3600',
-                    950: '#2E1B00',
-                },
-                violeta: {
-                    50: '#FEE2F6',
-                    100: '#FCC0EC',
-                    200: '#F980D9',
-                    300: '#F746C7',
-                    400: '#EF0BB2',
-                    500: '#AF0883',
-                    600: '#8D0669',
-                    700: '#6B0550',
-                    800: '#440333',
-                    900: '#220219',
-                    950: '#14010F',
-                },
-                rosado: {
-                    50: '#FCF2F5',
-                    100: '#FAE6EC',
-                    200: '#F4C8D5',
-                    300: '#EEAFC2',
-                    400: '#E891AB',
-                    500: '#E37898',
-                    600: '#D73D6B',
-                    700: '#AD244D',
-                    800: '#721833',
-                    900: '#3B0C1A',
-                    950: '#1E060D',
-                },
-            },
-        },
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+	],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
-    variants: {
-        extend: {
-            opacity: ['disabled'],
+    extend: {
+        fontFamily: {
+            sans: ['Nunito', ...defaultTheme.fontFamily.sans],
         },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        naranja: {
+            50: '#FFF2E5',
+            100: '#FFE5CC',
+            200: '#FFCA99',
+            300: '#FFB066',
+            400: '#FF9633',
+            500: '#FF7D00',
+            600: '#CC6300',
+            700: '#994A00',
+            800: '#663100',
+            900: '#331900',
+            950: '#190C00',
+        },
+        cremita: {
+            50: '#FFFDFA',
+            100: '#FFFBF5',
+            200: '#FFF7EB',
+            300: '#FFF4E5',
+            400: '#FFF0DB',
+            500: '#FFECD1',
+            600: '#FFC675',
+            700: '#FF9F1A',
+            800: '#B86B00',
+            900: '#5C3600',
+            950: '#2E1B00',
+        },
+        violeta: {
+            50: '#FEE2F6',
+            100: '#FCC0EC',
+            200: '#F980D9',
+            300: '#F746C7',
+            400: '#EF0BB2',
+            500: '#AF0883',
+            600: '#8D0669',
+            700: '#6B0550',
+            800: '#440333',
+            900: '#220219',
+            950: '#14010F',
+        },
+        rosado: {
+            50: '#FCF2F5',
+            100: '#FAE6EC',
+            200: '#F4C8D5',
+            300: '#EEAFC2',
+            400: '#E891AB',
+            500: '#E37898',
+            600: '#D73D6B',
+            700: '#AD244D',
+            800: '#721833',
+            900: '#3B0C1A',
+            950: '#1E060D',
+        },
+      },
+      opacity: ['disabled'],
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
-    plugins: [require('@tailwindcss/forms')],
+  },
+  plugins: [require("tailwindcss-animate"),require('@tailwindcss/forms')],
 }
+
+
