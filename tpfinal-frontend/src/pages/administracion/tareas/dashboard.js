199511881ae8e5e7dc38c81267f4a38e832c4ef4
Martina Coassin <martycoassin@gmail.com>
2023-10-17 12:44:34 -0300

    Update dashboard.js

--- tpfinal-frontend/src/pages/administracion/tareas/dashboard.js
+++ tpfinal-frontend/src/pages/administracion/tareas/dashboard.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from '@/lib/axios'
 import { CardTareaDashboard } from '../../../components/CardTarea'
+import Link from 'next/link'
 
 const fetchTareas = () => {
     return axios.get('/api/tareas').then(res => res.data)
@@ -15,6 +16,7 @@ export default function TareasDashboard() {
                 const data = await fetchTareas()
                 setTareas(data)
                 //console.log(data)
+
             } catch (error) {
                 console.error('Error al obtener tareas:', error)
             }
@@ -22,14 +24,22 @@ export default function TareasDashboard() {
         obtenerTareas()
     }, [])
 
+   
+
     return (
         <>
             {tareas ? (
-                <div className='flex flex-wrap flex-row'>
+                <div>
+                    <div className='flex flex-wrap flex-row'>
                     {tareas.map(tarea => (
                         <CardTareaDashboard key={tarea.id} tarea={tarea}></CardTareaDashboard>
                     ))}
                 </div>
+                <Link href="/administracion/tareas">
+                                Ver todas las tareas
+                            </Link>
+                </div>
+                
             ) : (
                 <div>cargando...</div>
             )}
