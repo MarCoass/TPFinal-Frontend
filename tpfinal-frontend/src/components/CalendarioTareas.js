bfffce76fc69d5f33b6c1abf9a2b2fee4c4cc011
Martina Coassin <martycoassin@gmail.com>
2023-10-18 14:20:06 -0300

    muestra los dias con tareas en el calendario

--- tpfinal-frontend/src/components/CalendarioTareas.js
+++ tpfinal-frontend/src/components/CalendarioTareas.js
@@ -1,14 +1,43 @@
 import { Calendar } from '@/components/ui/calendar'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from '@/lib/axios'
 
+const fetchTareas = () => {
+    return axios.get('/api/tareasInicio').then(res => res.data)
+}
 
 export default function CalendarioTareas() {
-    /* const [date, setDate] = (useState < Date) | (undefined > new Date()) */
+    const [tareas, setTareas] = useState()
+    const [seleccionadas, setSeleccionadas] = useState([])
+
+    useEffect(() => {
+        async function obtenerTareas() {
+            try {
+                const data = await fetchTareas()
+                setTareas(data.tareas)
+                /* console.log(data)  */
+            } catch (error) {
+                console.error('Error al obtener tareas:', error)
+            }
+        }
+        obtenerTareas()
+    }, [])
+
+    useEffect(() => {
+        if (tareas) {
+            let array = []
+            tareas.forEach(tarea => {
+                array.push(new Date(tarea.fecha_vencimiento))
+            })
+            setSeleccionadas(array)
+            /* console.log(array) */
+        }
+    },[tareas])
 
     return (
         <Calendar
-            mode="single"
-           
+            mode="multiple"
+            selected={seleccionadas}
             className="rounded-md border"
         />
     )
