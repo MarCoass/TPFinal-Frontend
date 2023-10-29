97aff9282fb6b91b25b6cb2fe193a55f6b81662f
MartinaRosalesF <martina.m.rosales@gmail.com>
2023-10-29 20:14:08 -0300

    intento traer datos del carrito

--- tpfinal-frontend/src/pages/tienda/carrito.js
+++ tpfinal-frontend/src/pages/tienda/carrito.js
@@ -1,7 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import AppLayout from "@/components/Layouts/AppLayout";
+import axios from '@/lib/axios'
+import CustomSpinner from '@/components/CustomSpinner'
+
+const fetchCarrito = (carrito) => {
+    return axios
+        .get(
+            `/carrito`, // Usa axios.get en lugar de fetch
+        )
+        .then(res => res.data)
+}
 
 export default function Catalogo() {
-
+    const [infoCarrito, setCarrito] = useState(null)
+    const router = useRouter()
+    const { carrito } = router.query
+    useEffect(() => {
+        if (carrito != null) {
+            async function obtenerDatos() {
+                try {
+                    const dataCarrito = await fetchCarrito(carrito)
+                    console.log(dataCarrito)
+                    setCarrito(dataCarrito)
+                } catch (error) {
+                    console.error('Hubo un problema obteniendo los datos: ', error)
+                }
+            }
+            obtenerDatos()
+        }
+        console.log(infoCarrito)
+    }, [carrito])
     return (
         <AppLayout>
             Habemus página de carrito
