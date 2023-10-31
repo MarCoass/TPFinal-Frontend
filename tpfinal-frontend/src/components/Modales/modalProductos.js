45a480bb099c3ce5a89629f2871809f3ee7185d0
Martina Coassin <martycoassin@gmail.com>
2023-10-31 10:40:52 -0300

    Update modalProductos.js

--- tpfinal-frontend/src/components/Modales/modalProductos.js
+++ tpfinal-frontend/src/components/Modales/modalProductos.js
@@ -470,7 +470,7 @@ export function ModalProductoUpdate({ idProducto }) {
     return (
         <>
             <AlertDialog>
-                <AlertDialogTrigger className="w-min rounded-full border-2 border-black bg-[#bc95d4] px-3 py-1.5 text-sm font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none ">
+                <AlertDialogTrigger className="w-min rounded-full border-2 border-black bg-lila-500 hover:bg-lila-600 px-3 py-1.5 text-sm font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none ">
                     <Pencil className="h-4 w-4 mx-2" />
 
                 </AlertDialogTrigger>
