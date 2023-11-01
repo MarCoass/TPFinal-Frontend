876905e42957da58d25c48646056ee76d73ae55b
Martina Coassin <76189888+MarCoass@users.noreply.github.com>
2023-11-01 17:21:44 -0300

    Update index.js

--- tpfinal-frontend/src/pages/administracion/index.js
+++ tpfinal-frontend/src/pages/administracion/index.js
@@ -17,16 +17,20 @@ const Dashboard = () => {
 
             <div className="py-12 ">
                 <div className=" grid grid-cols-6 grid-rows-5 gap-4">
-                    <div className="bg-rosado-200 col-span-4 row-span-3">
-                        Accesos rapidos
+                    <div className=" col-span-4 row-span-3">
+                        <div className="text-2xl font-bold ">
+                            Accesos rapidos
+                        </div>
                     </div>
 
                     <div className="col-span-2 row-span-5 col-start-5">
                         <TareasDashboard></TareasDashboard>
                         <CalendarioTareas></CalendarioTareas>
                     </div>
-                    <div className="col-span-4 row-span-2 row-start-4 bg-rosado-400">
-                        <p>Pedidos personalizados</p>
+                    <div className="col-span-4 row-span-2 row-start-4">
+                    <div className="text-2xl font-bold ">
+                            Pedidos personalizados
+                        </div>
                     </div>
                 </div>
             </div>
