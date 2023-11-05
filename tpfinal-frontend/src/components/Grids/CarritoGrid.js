53951770a1fd0c8634a6ca12947ff934ac68efc2
MartinaRosalesF <martina.m.rosales@gmail.com>
2023-11-05 19:40:19 -0300

    modifique el grid de carriot

--- tpfinal-frontend/src/components/Grids/CarritoGrid.js
+++ tpfinal-frontend/src/components/Grids/CarritoGrid.js
@@ -2,16 +2,28 @@ import React from "react";
 
 const CarritoGrid = ({ data }) => {
   return (
-    <div className="grid grid-cols-5 grid-rows-5 gap-4">
+    <div className="grid grid-cols-1 gap-4">
       {data.productos.map((producto) => (
-        <div key={producto.original.id} className="grid-item">
-          <img
-            src={producto.original.url_imagen}
-            alt={producto.original.nombre}
-            className="w-full h-auto"
-          />
-          <p>{producto.original.nombre}</p>
-          <p>Precio: ${producto.original.precio}</p>
+        <div key={producto.original.id} className="grid-item ">
+          <div className="grid grid-cols-5">
+            <img
+              src={producto.original.url_imagen}
+              alt={producto.original.nombre}
+              className=" h-auto"
+            />
+            <div className=" pl-4">
+              <p>Nombre: {producto.original.nombre}</p>
+            </div>
+            <div>
+              <p>Precio: ${producto.original.precio}</p>
+            </div>
+            <div>
+              cantidad producto
+            </div>
+            <div>
+              eliminar del carrito
+            </div>
+          </div>
         </div>
       ))}
     </div>
