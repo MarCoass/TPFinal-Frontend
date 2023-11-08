c0f211c105a58a4ad4da7d3765ca66cbe4951e02
MartinaRosalesF <martina.m.rosales@gmail.com>
2023-11-07 21:06:54 -0300

    se calcula total

--- tpfinal-frontend/src/components/Grids/CarritoGrid.js
+++ tpfinal-frontend/src/components/Grids/CarritoGrid.js
@@ -21,14 +21,29 @@ const CarritoGrid = ({ data }) => {
       const newCantidades = [...cantidades];
       newCantidades[index] -= 1;
       setCantidades(newCantidades);
-  
+
       // Llamada a la API para guardar la nueva cantidad en la base de datos.
     }
   };
 
+  const calculateTotalPrice = () => {
+    let total = 0;
+  
+    for (let i = 0; i < data.productos.length; i++) {
+      const producto = data.productos[i].original;
+      const cantidad = cantidades[i];
+      const precio = parseFloat(producto.precio);
+  
+      total += cantidad * precio;
+    }
+  
+    return total.toFixed(2); // Redondear a 2 decimales si es necesario
+  };
+
   return (
     <>
-      <div className="grid grid-cols-5 flex items-center">
+      <div className='border-b-2 mb-6 '>
+        <div className="grid grid-cols-5 flex items-center">
           <div> </div>
           <div className="flex justify-center font-bold">
             Nombre
@@ -40,40 +55,43 @@ const CarritoGrid = ({ data }) => {
             Cantidad
           </div>
           <div></div>
-      </div>
-      <div className="grid grid-cols-1 gap-4 mb-6 mt-6">
-        {data.productos.map((producto, i) => (
-          <div key={producto.original.id} className="grid-item ">
-            <div className="grid grid-cols-5 flex items-center ">
-              <div className="flex justify-center">
-                <img
-                  src={urlBase + producto.original.url_imagen}
-                  alt={producto.original.nombre}
-                  className="w-40 h-auto"
-                />
-              </div>
+        </div>
+        <div className="grid grid-cols-1 gap-4 mb-6 mt-6">
+          {data.productos.map((producto, i) => (
+            <div key={producto.original.id} className="grid-item ">
+              <div className="grid grid-cols-5 flex items-center ">
+                <div className="flex justify-center">
+                  <img
+                    src={urlBase + producto.original.url_imagen}
+                    alt={producto.original.nombre}
+                    className="w-40 h-auto"
+                  />
+                </div>
 
-              <div className="flex justify-center pl-4">
-                <p>{producto.original.nombre}</p>
-              </div>
-              <div className="flex justify-center">
-                <p>${producto.original.precio}</p>
-              </div>
-              <div className="flex flex-row gap-2 items-center justify-center">
-                <NeoButtonMini onClick={() => handleDecrement(i)}><Minus /></NeoButtonMini>
-                 {cantidades[i]}
-                <NeoButtonMini onClick={() => handleIncrement(i)}><Plus /></NeoButtonMini>
-              </div>
-              <div className="flex justify-center">
-                <NeoButtonMini><Trash2 /></NeoButtonMini>
+                <div className="flex justify-center pl-4">
+                  <p>{producto.original.nombre}</p>
+                </div>
+                <div className="flex justify-center">
+                  <p>${producto.original.precio}</p>
+                </div>
+                <div className="flex flex-row gap-2 items-center justify-center">
+                  <NeoButtonMini onClick={() => handleDecrement(i)}><Minus /></NeoButtonMini>
+                  {cantidades[i]}
+                  <NeoButtonMini onClick={() => handleIncrement(i)}><Plus /></NeoButtonMini>
+                </div>
+                <div className="flex justify-center">
+                  <NeoButtonMini><Trash2 /></NeoButtonMini>
+                </div>
               </div>
             </div>
-          </div>
 
-        ))}
+          ))}
+        </div>
+      </div>
+      <div>
+        <p>Total: ${calculateTotalPrice()} </p>
       </div>
     </>
-
 
   );
 };
