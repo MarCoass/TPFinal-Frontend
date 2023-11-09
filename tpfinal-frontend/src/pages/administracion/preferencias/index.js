d51fe8af81344fd681db4c6e5c45f95b731c5019
Martina Coassin <martycoassin@gmail.com>
2023-11-09 11:37:15 -0300

    funciona el update tienda

--- tpfinal-frontend/src/pages/administracion/preferencias/index.js
+++ tpfinal-frontend/src/pages/administracion/preferencias/index.js
@@ -1,7 +1,67 @@
 import AdminLayout from '@/components/Layouts/AdminLayout'
 import Head from 'next/head'
+import axios from '@/lib/axios'
+import { useState, useEffect } from 'react'
+import Input from '@/components/Input'
+import { NeoButton } from '../../../components/Button'
+import { handleUpdateParametros } from '../../../lib/handleUpdate'
+
+const fetchParametros = () => {
+    return axios.get('/api/parametros').then(res => res.data)
+}
 
 const Dashboard = () => {
+    const [parametros, setParametros] = useState()
+    const [parametrosArray, setParametrosArray] = useState()
+
+    useEffect(() => {
+        async function obtenerParametros() {
+            try {
+                const data = await fetchParametros()
+                setParametros(data)
+            } catch (error) {
+                console.error('Error al obtener parametros:', error)
+            }
+        }
+        obtenerParametros()
+    }, [])
+
+    useEffect(() => {
+        if (parametros != null) {
+            let array = {}
+            parametros.forEach(parametro => {
+                array[parametro.nombre] = parametro.valor
+            })
+            setParametrosArray(array)
+        }
+    }, [parametros])
+
+    const handleInputChange = (paramName, value) => {
+        // Create a copy of the current state
+        const updatedParametrosArray = { ...parametrosArray }
+        // Update the value for the specific parameter
+        updatedParametrosArray[paramName] = value
+        // Update the state with the new values
+        setParametrosArray(updatedParametrosArray)
+    }
+
+    const handleSubmit = async event => {
+        event.preventDefault()
+        try {
+            const formData = new FormData()
+
+            // Itera a través de los valores de parametrosArray y agrégalos al FormData
+            for (const paramName in parametrosArray) {
+                formData.append(paramName, parametrosArray[paramName])
+            }
+
+            handleUpdateParametros('/api/parametros/update', formData);
+        } catch (error) {
+            console.error('Error al actualizar los parámetros:', error)
+            alert('Hubo un error al actualizar los datos. Inténtalo de nuevo.')
+        }
+    }
+
     return (
         <AdminLayout
             header={
@@ -14,9 +74,200 @@ const Dashboard = () => {
             </Head>
 
             <div className="py-12 ">
-                <div className=" grid grid-cols-6 grid-rows-5 gap-4">
-                   
-                </div>
+                {parametrosArray ? (
+                    <form onSubmit={handleSubmit}>
+                        <div className=" grid grid-cols-2 gap-4 mx-4">
+                            <div className="rounded-[5px] border-2 border-black  font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
+                                <p className="font-bold text-lg border-b-2 border-black bg-rosado-400 p-3">
+                                    Redes
+                                </p>
+                                <div>
+                                    <p className="font-bold">Instagram</p>
+                                    <div>
+                                        <label htmlFor="instagram_nombre">
+                                            Username
+                                        </label>
+                                        <Input
+                                            id="instagram_nombre"
+                                            value={
+                                                parametrosArray[
+                                                    'instagram_nombre'
+                                                ]
+                                            }
+                                            onChange={e =>
+                                                handleInputChange(
+                                                    'instagram_nombre',
+                                                    e.target.value,
+                                                )
+                                            }
+                                        />
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor="instagram_url">URL</label>
+                                        <Input
+                                            id="instagram_url"
+                                            value={
+                                                parametrosArray['instagram_url']
+                                            }
+                                            onChange={e =>
+                                                handleInputChange(
+                                                    'instagram_url',
+                                                    e.target.value,
+                                                )
+                                            }
+                                        />
+                                    </div>
+                                    <p className="font-bold">WhatsApp</p>
+                                    <div>
+                                        <label htmlFor="whatsapp">WhatsApp</label>
+                                        <Input
+                                            id="whatsapp"
+                                            value={parametrosArray['whatsapp']}
+                                            onChange={e =>
+                                                handleInputChange(
+                                                    'whatsapp',
+                                                    e.target.value,
+                                                )
+                                            }
+                                        />
+                                    </div>
+
+                                    <p className="font-bold">
+                                        Horario de atencion
+                                    </p>
+                                    <div>
+                                        <label htmlFor="horario_atencion_apertura">
+                                            Desde:{' '}
+                                        </label>
+                                        <Input
+                                            id="horario_atencion_apertura"
+                                            value={
+                                                parametrosArray[
+                                                    'horario_atencion_apertura'
+                                                ]
+                                            }
+                                            onChange={e =>
+                                                handleInputChange(
+                                                    'horario_atencion_apertura',
+                                                    e.target.value,
+                                                )
+                                            }
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="horario_atencion_cierre">
+                                            Hasta:{' '}
+                                        </label>
+                                        <Input
+                                            id="horario_atencion_cierre"
+                                            value={
+                                                parametrosArray[
+                                                    'horario_atencion_cierre'
+                                                ]
+                                            }
+                                            onChange={e =>
+                                                handleInputChange(
+                                                    'horario_atencion_cierre',
+                                                    e.target.value,
+                                                )
+                                            }
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="rounded-[5px] border-2 border-black  font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
+                                <p className="font-bold text-lg border-b-2 border-black bg-rosado-400 p-3">
+                                    Tienda
+                                </p>
+                                <div>
+                                    <div>
+                                        <label htmlFor="valor_senia">
+                                            Valor de la seña:
+                                        </label>
+                                        <Input
+                                            type="number"
+                                            id="valor_senia"
+                                            value={
+                                                parametrosArray['valor_senia']
+                                            }
+                                            onChange={e =>
+                                                handleInputChange(
+                                                    'valor_senia',
+                                                    e.target.value,
+                                                )
+                                            }
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="demora_cotizacion">
+                                            Demora cotizacion:
+                                        </label>
+                                        <Input
+                                            id="demora_cotizacion"
+                                            value={
+                                                parametrosArray[
+                                                    'demora_cotizacion'
+                                                ]
+                                            }
+                                            onChange={e =>
+                                                handleInputChange(
+                                                    'demora_cotizacion',
+                                                    e.target.value,
+                                                )
+                                            }
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="demora_trabajo">
+                                            Demora trabajo:
+                                        </label>
+                                        <Input
+                                            id="demora_trabajo"
+                                            value={
+                                                parametrosArray[
+                                                    'demora_trabajo'
+                                                ]
+                                            }
+                                            onChange={e =>
+                                                handleInputChange(
+                                                    'demora_trabajo',
+                                                    e.target.value,
+                                                )
+                                            }
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="pedidos_abiertos">
+                                            Pedidos abiertos:
+                                        </label>
+                                        <input
+                                            type="radio"
+                                            id="pedidos_abiertos"
+                                            value={
+                                                parametrosArray[
+                                                    'pedidos_abiertos'
+                                                ]
+                                            }
+                                            onChange={e =>
+                                                handleInputChange(
+                                                    'pedidos_abiertos',
+                                                    e.target.value,
+                                                )
+                                            }
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mt-6">
+                            {' '}
+                            <NeoButton type="submit">Guardar cambios</NeoButton>
+                        </div>
+                    </form>
+                ) : (
+                    <p>Cargando...</p>
+                )}
             </div>
         </AdminLayout>
     )
