677716aa2da2267a32f184c99e062afdaf932fb0
Martina Coassin <martycoassin@gmail.com>
2023-11-24 03:48:17 -0300

    mas cambios en el home

--- tpfinal-frontend/src/pages/index.js
+++ tpfinal-frontend/src/pages/index.js
@@ -66,7 +66,7 @@ export default function Home() {
                         </p>
                     </div>
                 </div>
-                <div className=" p-5 pb-0 bg-naranja-100 flex flex-col justify-center align-middle md:flex-wrap md:flex-row md:justify-evenly  md:content-stretch gap-4 border-b-2 border-black">
+                <div className="py-10   bg-naranja-100 flex flex-col justify-center align-middle md:flex-wrap md:flex-row md:justify-evenly  md:content-stretch gap-4 border-b-2 border-black">
                     <CardInicio
                         img="/images/personalizadas.png"
                         titulo="Uñas personalizadas"
@@ -110,6 +110,129 @@ export default function Home() {
                             Enviar mensaje
                         </Link>
                     </CardInicio>
+                </div>
+                <div className="pl-5 pt-10 bg-lila-100 flex flex-col justify-center align-middle md:grid md:grid-cols-3 gap-4 border-b-2 border-black">
+                    <div className="pr-5 col-span-2 flex flex-col self-center m-10">
+                        <p className="text-3xl md:text-5xl mb-3">
+                            Informacion sobre las press on
+                        </p>
+                        <p className="text-lg">
+                            Son uñas postizas hechas con tips de gel, los mismos
+                            que se usan para las SoftGel.
+                            <br />
+                            Reutilizables, cuidandolas se pueden reutilizar las
+                            veces que quieras.
+                            <br />
+                            Uñas listas en poco tiempo, la aplicacion es facil y
+                            rapida, lo haces vos misma desde tu casa.
+                            <br />
+                            Diferente duracion, podes usar pegamento liquido o
+                            stickers doble faz segun la duracion necesaria.
+                        </p>
+                    </div>
+                    <Image
+                        className="col-start-3"
+                        src="/images/rojas.png"
+                        width={500}
+                        height={500}
+                        alt="Press on rojas con efecto sangre"
+                    />
+                </div>
+                <div className=" border-b-2 border-black lg:grid lg:grid-cols-3 lg:divide-x-2 lg:divide-y-0 divide-black divide-y-2">
+                    <div className="py-10 px-5 bg-violeta-100">
+                        <p className="text-4xl">¿Como pegarlas?</p>
+                        <div className="flex flex-col gap-3 py-4">
+                            <ul>
+                                Preparar la uña
+                                <li className="pl-5">
+                                    Repujar las cuticulas con el palito de
+                                    naranjo.
+                                </li>
+                                <li className="pl-5">
+                                    Limar suavemente la superficie de la uña.
+                                </li>
+                                <li className="pl-5">
+                                    Limpiar el polvo con un algodon y alcohol.
+                                </li>
+                            </ul>
+                            <ul>
+                                Con pegamento liquido:
+                                <li className="pl-5">
+                                    Colocar una gota de pegamento en el tip o en
+                                    la uña.
+                                </li>
+                                <li className="pl-5">
+                                    Apoyar el tip apoyando desde la cuticula
+                                    hasta la punta.
+                                </li>
+                                <li className="pl-5">
+                                    Presionar unos segundos.
+                                </li>
+                            </ul>
+                            <ul>
+                                Con stickers doble faz:
+                                <li className="pl-5">
+                                    Colocar el sticker del tamaño adecuado en el
+                                    tip.
+                                </li>
+                                <li className="pl-5">
+                                    Quitar el plastico y colocar el tip en la
+                                    uña.
+                                </li>
+                                <li className="pl-5">
+                                    Presionar unos segundos.
+                                </li>
+                            </ul>
+                        </div>
+
+                        <p className="my-4">
+                            Consejo: Deja los pulgares para el final ya que son
+                            los dedos que mas se usan.
+                        </p>
+                    </div>
+                    <div className="py-10 px-5 bg-naranja-200">
+                        <p className="text-4xl">¿Como sacarlas?</p>
+                        <ul>
+                            <li>
+                                Sumergir las uñas entre 10 y 15 minutos en agua
+                                tibia/caliente.
+                            </li>
+                            <li>
+                                Con el palito de naranjo empujar desde la zona
+                                de cuticulas.
+                            </li>
+                            <li>
+                                Limpia los restos de pegamento con quitaesmalte
+                                o limando suavemente.
+                            </li>
+                        </ul>
+                        <p>
+                            Consejo: El dia anterior podes usar cremas o aceites
+                            hidratantes para que el pegamento vaya perdiendo
+                            efecto. Si necesitas mucha fuerza para despegarlas,
+                            sumergi mas tiempo las uñas en agua.
+                        </p>
+                    </div>
+                    <div className="py-10 px-5 bg-lila-300">
+                        <p className="text-4xl">¿Como cuidarlas?</p>
+                        <ul>
+                            <li>No mordelas.</li>
+                            <li>
+                                Retirarlas correctamente, arrancarlas puede
+                                lastimar la uña natural.
+                            </li>
+                            <li>No usarlas como herramientas.</li>
+                            <li>
+                                Usar guantes cuando uses productos de limpieza.
+                            </li>
+                        </ul>
+                        <p>
+                            Consejo: El dia anterior podes usar cremas o aceites
+                            hidratantes para que el pegamento vaya perdiendo
+                            efecto. Si necesitas mucha fuerza para despegarlas,
+                            sumergi mas tiempo las uñas en agua.
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
