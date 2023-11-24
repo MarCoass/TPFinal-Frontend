eaaed332960c9384a4d09970556511cb07e175b6
Martina Coassin <martycoassin@gmail.com>
2023-11-23 23:19:59 -0300

    Update SideNavigation.js

--- tpfinal-frontend/src/components/Layouts/SideNavigation.js
+++ tpfinal-frontend/src/components/Layouts/SideNavigation.js
@@ -40,55 +40,65 @@ export function NeoSideNavigation({ active, setActive, children }) {
                 style={{
                     transform: `translateX(${isVisible ? '0' : '-300px'})`,
                 }}
-                className=" z-10 h-full w-[300px] border-2 border-black bg-rosado-400 font-bold transition-transform duration-300">
-                <div className="p-5 justify-center flex items-center border-b-2 border-black">
-                    <Link href="/">
-                        <ApplicationLogo className="block h-16 w-auto fill-current text-violeta-500" />
-                    </Link>
-                </div>
-                <div className="flex flex-col  ">
-                    <NavLink
-                        href="/administracion/"
-                        active={router.pathname === '/administracion'}>
-                        Inicio
-                    </NavLink>
-                    <NavLink
-                        href="/administracion/productos"
-                        active={
-                            router.pathname === '/administracion/productos'
-                        }>
-                        Productos
-                    </NavLink>
-                    <NavLink
-                        href="/administracion/pedidos"
-                        active={router.pathname === '/'}>
-                        Pedidos personalizados
-                    </NavLink>
-                    <NavLink
-                        href="/administracion/insumos"
-                        active={router.pathname === '/administracion/insumos'}>
-                        Insumos
-                    </NavLink>
-                    <NavLink
-                        href="/administracion/tareas"
-                        active={router.pathname === '/administracion/tareas'}>
-                        Tareas
-                    </NavLink>
+                className="grid content-between z-10 h-full w-[300px] border-2 border-black bg-rosado-400 font-bold transition-transform duration-300">
+                <div>
+                    <div className="p-5 justify-center flex items-center border-b-2 border-black">
+                        <Link href="/">
+                            <ApplicationLogo className="block h-16 w-auto fill-current text-violeta-500" />
+                        </Link>
+                    </div>
+                    <div className="flex flex-col  ">
+                        <NavLink
+                            href="/administracion/"
+                            active={router.pathname === '/administracion'}>
+                            Inicio
+                        </NavLink>
+                        <NavLink
+                            href="/administracion/productos"
+                            active={
+                                router.pathname === '/administracion/productos'
+                            }>
+                            Productos
+                        </NavLink>
+                        <NavLink
+                            href="/administracion/pedidos"
+                            active={router.pathname === '/'}>
+                            Pedidos personalizados
+                        </NavLink>
+                        <NavLink
+                            href="/administracion/insumos"
+                            active={
+                                router.pathname === '/administracion/insumos'
+                            }>
+                            Insumos
+                        </NavLink>
+                        <NavLink
+                            href="/administracion/tareas"
+                            active={
+                                router.pathname === '/administracion/tareas'
+                            }>
+                            Tareas
+                        </NavLink>
 
-                    <NavLink
-                        href="/administracion/proveedores"
-                        active={
-                            router.pathname === '/administracion/proveedores'
-                        }>
-                        Proveedores
-                    </NavLink>
-                    <NavLink
-                        href="/administracion/clientes"
-                        active={router.pathname === '/administracion/clientes'}>
-                        Clientes
-                    </NavLink>
+                        <NavLink
+                            href="/administracion/proveedores"
+                            active={
+                                router.pathname ===
+                                '/administracion/proveedores'
+                            }>
+                            Proveedores
+                        </NavLink>
+                        <NavLink
+                            href="/administracion/clientes"
+                            active={
+                                router.pathname === '/administracion/clientes'
+                            }>
+                            Clientes
+                        </NavLink>
+                    </div>
                 </div>
-                <div className="flex flex-col border-t-2 border-black">
+
+                <div className="flex flex-col border-t-2 border-black ">
                     <NavLink href="/administracion/preferencias">
                         Preferencias
                     </NavLink>
