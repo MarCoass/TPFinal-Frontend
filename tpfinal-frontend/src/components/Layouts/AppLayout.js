e55d707cd5e578f591f7826488e3aff718a31396
MartinaRosalesF <martina.m.rosales@gmail.com>
2023-11-27 19:38:29 -0300

    mejore pagina de producto

--- tpfinal-frontend/src/components/Layouts/AppLayout.js
+++ tpfinal-frontend/src/components/Layouts/AppLayout.js
@@ -10,13 +10,13 @@ const AppLayout = ({ header, children, footer }) => {
 
             {/* Page Heading */}
             <header className="bg-lila-500 shadow">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="bg-lila-500 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     {header}
                 </div>
             </header>
 
             {/* Page Content */}
-            <main>{children}</main>
+            <main className="bg-lila-300 min-h-screen">{children}</main>
 
             
         </div>
--- tpfinal-frontend/src/pages/producto/[producto].js
+++ tpfinal-frontend/src/pages/producto/[producto].js
@@ -36,7 +36,7 @@ export default function infoProducto({ params }) {
     }, [producto])
 
     const handleAddToCart = async (id, cantidad) => {
-        if(user){
+        if (user) {
             try {
                 const responseStock = await axios.get(`/api/verificar-stock/${JSON.stringify({ id_producto: id, cantidad: cantidad })}`);
                 if (responseStock.data && responseStock.data.stock) {
@@ -84,11 +84,11 @@ export default function infoProducto({ params }) {
 
     };
 
-    const handleAgregarFavorito = async (id) =>{
-        if(user){
-            const responseAdd = await axios.post('api/favorito-agregar', { id_producto: id});
-            if (responseAdd ) {
-                if(!responseAdd.data.repetido){
+    const handleAgregarFavorito = async (id) => {
+        if (user) {
+            const responseAdd = await axios.post('api/favorito-agregar', { id_producto: id });
+            if (responseAdd) {
+                if (!responseAdd.data.repetido) {
                     swal({
                         icon: 'success',
                         title: 'Producto agregado a favoritos.',
@@ -131,69 +131,68 @@ export default function infoProducto({ params }) {
                 },
             })
         }
-       
+
     }
     // 
     const urlBase = process.env.NEXT_PUBLIC_BACKEND_URL + '/storage';
     return (
         <AppLayout>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="flex justify-center container bg-white overflow-hidden shadow-sm sm:rounded-lg sm:px-6 lg:px-8 ">
-                        {infoProducto ? (
-                            <div className=" p-6 bg-white border-b border-gray-200">
+            <div className="h-[40rem] pt-12 ">
+                <div className="max-w-7xl h-[25rem] bg-lila-500 border border-2 border-black rounded-[5px] mx-auto sm:px-6 lg:px-8">
+                    {infoProducto ? (
+                        <div className="pt-8 flex justify-center">
 
-                                <div className='min-w-2xl'> <img
-                                    alt={infoProducto.descripcion}
-                                    className="h-40 rounded-2xl w-full object-cover"
-                                    src={urlBase + infoProducto.url_imagen}></img></div>
+                            <div className='min-w-2xl'> <img
+                                alt={infoProducto.descripcion}
+                                className="h-[18rem] w-[24rem] border border-black border-2 rounded-2xl w-52 object-cover shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]"
+                                src={urlBase + infoProducto.url_imagen}></img></div>
 
-                                <div>
-                                    <div>
-                                        <div className=''><p className='text-xl font-bold'>{infoProducto.nombre}</p></div>
-                                        <p className='text-lg'>${infoProducto.precio}</p></div>
-                                    <div className='flex flex-row gap-4 '>
-                                        {/* <div className='cantidad-producto flex flex-row itens-center border-2 max-w-min rounded-full'><button name='quitar una unidad de producto' className='flex-items-center text-xs bg-gray-300 p-1 rounded-full h-min leading-none'>-</button><input className='border-0 w-20' value={1}></input><button  name='sumar una unidad de producto' >+</button></div> */}
-                                        <Button className='inline-flex items-center justify-center px-4 py-2 ' onClick={() => handleAddToCart(infoProducto.id, 1)}>Agregar al carrito</Button>
-                                        <div className="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
-                                            <button title="agregar a favoritos" onClick={() => handleAgregarFavorito(infoProducto.id)}>
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    className="h-6 w-6 group-hover:opacity-70"
-                                                    fill="none"
-                                                    viewBox="0 0 24 24"
-                                                    stroke="gray">
-                                                    <path
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        strokeWidth="2"
-                                                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </div>
-                                    </div>
-
-                                    <div className='info-producto'>
-                                        <p>Descripción:</p>
-                                        <div><p className='text-lg'>disponibilidad de entrega: {infoProducto.ciudad.nombre}</p></div>
-                                        <div><p className='text-lg'>{infoProducto.descripcion}</p></div>
-                                        <p>Largo: {infoProducto.set.tip.largo}cm ({infoProducto.set.tip.tags})</p>
-                                        <p>Categoría: {infoProducto.set.categoria_set.nombre}</p>
+                            <div className='flex flex-col ml-4'>
+                                <div className='flex flex-col'>
+                                    <div className='flex justify-center mb-3'><p className='text-3xl font-bold'>{infoProducto.nombre}</p></div>
+                                    <div className='flex-justify-center mb-6'><p className='text-lg font-semibold'>{infoProducto.descripcion}</p></div>
+                                </div>
+                                
+                                <div className='info-producto'>
+                                    <div><p className='text-lg font-semibold'><b>Disponibilidad de entrega:</b> {infoProducto.ciudad.nombre}</p></div>
+                                    <p className='text-lg font-semibold'><b>Largo:</b> {infoProducto.set.tip.largo}cm</p>
+                                    <p className='text-lg font-semibold'><b>Categoría:</b> {infoProducto.set.categoria_set.nombre}</p>
+                                    <p className='text-lg font-semibold'><b>Precio: </b>${infoProducto.precio}</p>
+                                </div>
+                                <div className='flex flex-row gap-4 mt-4'>
+                                    
+                                    <Button className='inline-flex items-center px-4 py-2 ' onClick={() => handleAddToCart(infoProducto.id, 1)}>Agregar al carrito</Button>
+                                    <div className="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
+                                        <button title="agregar a favoritos" onClick={() => handleAgregarFavorito(infoProducto.id)}>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                className="h-6 w-6 group-hover:opacity-70"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke="black">
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
+                                                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                                                />
+                                            </svg>
+                                        </button>
                                     </div>
                                 </div>
 
                             </div>
 
-                        ) : (
-                            <div>
-                                <CustomSpinner
-                                    mensaje={'Cargando producto...'}>
-                                </CustomSpinner>
-                            </div>
-                        )}
-                    </div>
+                        </div>
+
+                    ) : (
+                        <div>
+                            <CustomSpinner
+                                mensaje={'Cargando producto...'}>
+                            </CustomSpinner>
+                        </div>
+                    )}
                 </div>
             </div>
         </AppLayout>
