547790086c3833e385b220eb07e79cff9f62b897
MartinaRosalesF <martina.m.rosales@gmail.com>
2023-11-27 17:43:48 -0300

    ta raro pero arreglado jeje

--- tpfinal-frontend/src/components/Grids/CarritoGrid.js
+++ tpfinal-frontend/src/components/Grids/CarritoGrid.js
@@ -3,13 +3,6 @@ import { NeoButtonChico, NeoButtonMini } from "../Button";
 import { Plus, Trash2, Minus } from 'lucide-react'
 import axios from '@/lib/axios'
 
-const eliminarProducto = () => {
-  return axios
-    .get(
-      `eliminar-producto`, // Usa axios.get en lugar de fetch
-    )
-    .then(res => res.data)
-}
 
 const CarritoGrid = ({ obtenerDatos, data, obtenerPrecioTotal }) => {
   const urlBase = process.env.NEXT_PUBLIC_BACKEND_URL + '/storage/';
@@ -42,13 +35,14 @@ const CarritoGrid = ({ obtenerDatos, data, obtenerPrecioTotal }) => {
 
   const handleRemoveProduct = async (idProducto) => {
     try {
-      const productoEncontrado = cantidades.find((producto)=> producto.id_producto === idProducto)
-      console.log(idProducto)
+      const productoEncontrado = cantidades.find((producto) => producto.id_producto === idProducto)
       // Llamada a la API para eliminar el producto
-      const response = await axios.post('/eliminar-producto',productoEncontrado);
+      const response = await axios.post('/eliminar-producto', productoEncontrado);
       console.log('Producto eliminado:', response.data);
-      if(response){
-        console.log(response)
+      if (response) {
+        console.log(productoEncontrado)
+        setCantidades(cantidades.filter((producto) => producto != productoEncontrado))
+        console.log(cantidades)
         obtenerDatos()
       }
       // Manejo de la respuesta si es necesario
@@ -69,7 +63,6 @@ const CarritoGrid = ({ obtenerDatos, data, obtenerPrecioTotal }) => {
 
       // Llamada a la API para actualizar el carrito
       const response = await axios.post('/actualizar-carrito', { id_productos: cantidadesActualizadas });
-      console.log('Carrito actualizado:', response.data);
       // Manejo de la respuesta si es necesario
     } catch (error) {
       console.error('Error al actualizar el carrito:', error);
@@ -79,16 +72,21 @@ const CarritoGrid = ({ obtenerDatos, data, obtenerPrecioTotal }) => {
 
   const calculateTotalPrice = () => {
     let total = 0;
-    if(data.productos){
+    if (data.productos) {
       for (let i = 0; i < data.productos.length; i++) {
         const producto = data.productos[i].original;
-        const cantidad = cantidades[i].cantidad;
-        const precio = parseFloat(producto.precio);
+        if(cantidades[i]){
+          const cantidad = cantidades[i].cantidad;
+          const precio = parseFloat(producto.precio);
   
-        total += cantidad * precio;
+          total += cantidad * precio;
+        } else {
+          total += 0
+        }
+      
       }
     }
-    
+
     // setPrecioTotal(total.toFixed(2))
     return total.toFixed(2); // Redondear a 2 decimales si es necesario
   };
@@ -110,45 +108,49 @@ const CarritoGrid = ({ obtenerDatos, data, obtenerPrecioTotal }) => {
           <div></div>
         </div>
         {
-          data.productos && data.productos.length > 0 ? 
-          (<div className="grid grid-cols-1 gap-4 mb-6 mt-6">
-            {data.productos.map((producto, i) => (
-              <div key={producto.original.id} className="grid-item ">
-                <div className="grid grid-cols-5 flex items-center ">
-                  <div className="flex justify-center">
-                    <img
-                      src={urlBase + producto.original.url_imagen}
-                      alt={producto.original.nombre}
-                      className="w-40 h-auto"
-                    />
-                  </div>
+          data.productos && data.productos.length > 0 ?
+            (<div className="grid grid-cols-1 gap-4 mb-6 mt-6">
+              {data.productos.map((producto, i) => (
+                <div key={producto.original.id} className="grid-item ">
+                  <div className="grid grid-cols-5 flex items-center ">
+                    <div className="flex justify-center">
+                      <img
+                        src={urlBase + producto.original.url_imagen}
+                        alt={producto.original.nombre}
+                        className="w-40 h-auto"
+                      />
+                    </div>
 
-                  <div className="flex justify-center pl-4">
-                    <p>{producto.original.nombre}</p>
+                    <div className="flex justify-center pl-4">
+                      <p>{producto.original.nombre}</p>
+                    </div>
+                    <div className="flex justify-center">
+                      <p>${producto.original.precio}</p>
+                    </div>
+                    {cantidades[i] ? (
+                      <div className="flex flex-row gap-2 items-center justify-center">
+                        <NeoButtonMini onClick={() => handleDecrement(i)}><Minus /></NeoButtonMini>
+                        {cantidades[i].cantidad}
+                        <NeoButtonMini onClick={() => handleIncrement(i)}><Plus /></NeoButtonMini>
+                      </div>
+                    ) : (
+                      null
+                    )}
+                    <div className="flex justify-center">
+                      <NeoButtonMini onClick={() => handleRemoveProduct(producto.original.id)}><Trash2 /></NeoButtonMini>
+                    </div>
                   </div>
-                  <div className="flex justify-center">
-                    <p>${producto.original.precio}</p>
-                  </div>
-                  <div className="flex flex-row gap-2 items-center justify-center">
-                    <NeoButtonMini onClick={() => handleDecrement(i)}><Minus /></NeoButtonMini>
-                    {cantidades[i].cantidad}
-                    <NeoButtonMini onClick={() => handleIncrement(i)}><Plus /></NeoButtonMini>
-                  </div>
-                  <div className="flex justify-center">
-                    <NeoButtonMini onClick={() => handleRemoveProduct(producto.original.id)}><Trash2 /></NeoButtonMini>
-                  </div>
+                </div>
+
+              ))}
+            </div>) : (
+              <div className='mb-8 mt-8'>
+                <div className="flex justify-center">
+                  No hay productos en el carrito
                 </div>
               </div>
 
-            ))}
-          </div>) : (
-            <div className='mb-8 mt-8'>
-              <div className="flex justify-center">
-                No hay productos en el carrito
-              </div>
-            </div>
-
-          )
+            )
         }
 
       </div>
